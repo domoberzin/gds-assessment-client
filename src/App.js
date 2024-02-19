d24eef3ad3e710de50d2febf02f6bc691743ddf9
Dominic Berzin <berzindominic@gmail.com>
2024-02-19 11:04:14 +0800

    Modify client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = React.useState(null);
+  const [firstNumber, setFirstNumber] = React.useState(0);
+  const [secondNumber, setSecondNumber] = React.useState(0);
+  const [error, setError] = React.useState(null);
+
+  const getAddResult = () => {
+    setResult(null);
+    if (firstNumber === '' || secondNumber === '') {
+      setError('Please enter both numbers');
+      return;
+    }
+    
+    setError(null);
+
+    fetch('api/v1/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({a: Number(firstNumber), b: Number(secondNumber)})
+    })
+    .then(response => response.json())
+    .then(data => setResult(data.result))
+    .catch(error => console.error('Error:', error));
+  }
+
+  const getSubResult = () => {
+    setResult(null);
+    if (firstNumber === '' || secondNumber === '') {
+      setError('Please enter both numbers');
+      return;
+    }
+
+    setError(null);
+
+    fetch('api/v1/subtract', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({a: Number(firstNumber), b: Number(secondNumber)})
+    })
+    .then(response => response.json())
+    .then(data => setResult(data.result))
+    .catch(error => console.error('Error:', error));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: '20px', textAlign: 'center' }}>
+      <div style={{ padding: '20px' }}>
+        <label>First Number: <input type="number" onChange={(e) => setFirstNumber(e.target.value)} /></label>
+      </div>
+      <div style={{ padding: '20px' }}>
+        <label>Second Number: <input type="number" onChange={(e) => setSecondNumber(e.target.value)} /></label>
+      </div>
+      <button onClick={getAddResult}>Add</button>
+      <button onClick={getSubResult}>Subtract</button>
+      {result !== null && <p>Result: {result}</p>}
+      {error !== null && <p>Error: {error}</p>}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,100 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ result: 3 }),
+  })
+);
+
+beforeEach(() => {
+  fetch.mockClear();
+});
+
+test('renders input fields and buttons', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByLabelText('First Number:')).toBeInTheDocument();
+  expect(screen.getByLabelText('Second Number:')).toBeInTheDocument();
+  expect(screen.getByText('Add')).toBeInTheDocument();
+  expect(screen.getByText('Subtract')).toBeInTheDocument();
+});
+
+test('displays error when inputs are empty and add button is clicked', async () => {
+  render(<App />);
+
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ result: 0 }),
+    })
+  );
+  
+  fireEvent.click(screen.getByText('Add'));
+  await waitFor(() => expect(screen.getByText('Result: 0')).toBeInTheDocument());
+
+  expect(fetch).toHaveBeenCalledTimes(1);
+});
+
+test('displays result when valid inputs are provided and add button is clicked', async () => {
+  render(<App />);
+
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ result: 3 }),
+    }));
+
+  userEvent.type(screen.getByLabelText('First Number:'), '1');
+  userEvent.type(screen.getByLabelText('Second Number:'), '2');
+  fireEvent.click(screen.getByText('Add'));
+  await waitFor(() => expect(screen.getByText('Result: 3')).toBeInTheDocument());
+
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch).toHaveBeenCalledWith('api/v1/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({a: 1, b: 2})
+  });
+});
+
+
+test('default 0 on empty input for subtract', async () => {
+  render(<App />);
+
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ result: 0 }),
+    })
+  );
+  
+  fireEvent.click(screen.getByText('Subtract'));
+  await waitFor(() => expect(screen.getByText('Result: 0')).toBeInTheDocument());
+
+  expect(fetch).toHaveBeenCalledTimes(1);
+
+});
+
+test('displays result when valid inputs are provided and subtract button is clicked', async () => {
+  render(<App />);
+
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ result: 3 }),
+    })
+  );
+
+  userEvent.type(screen.getByLabelText('First Number:'), '1');
+  userEvent.type(screen.getByLabelText('Second Number:'), '2');
+  fireEvent.click(screen.getByText('Subtract'));
+  await waitFor(() => expect(screen.getByText('Result: 3')).toBeInTheDocument());
+
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch).toHaveBeenCalledWith('api/v1/subtract', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({a: 1, b: 2})
+  });
 });
